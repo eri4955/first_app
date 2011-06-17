c764083f96e9fd8610ecf575cf5a59e96978bb2c
eri <mail@govision.eu>
2011-06-17 13:10:35 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7164a8d2e4952418a124fe7cab69ed9df9367fe66242f8438939dcbe8bbfece5abda627cd4515cfb2506e5d7355e215b90456ebb7ff12bb4b5f38b93119c8cb8'
